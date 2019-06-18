441c0fadb4fa9f4a35eec3892bfa99dec65cb1ea
Komissarov Andrey <komissar.off.andrey@gmail.com>
2019-06-18 23:46:35 +0300

    fixes

--- k2/aeon/responses/client/client.py
+++ k2/aeon/responses/client/client.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
+
+try:
+    from ujson import loads
+except ImportError:
+    from json import loads
+
 from k2.aeon.responses.base_response import Response
+
 
 class ClientResponse(Response):
 
-	@property
-	def ok(self):
-		return 200 <= self.code < 300
+    @property
+    def ok(self):
+        return 200 <= self.code < 300
 
-	@property
-	def text(self):
-		if isinstance(self._data, str):
-			return self._data
-		try:
-			return self._data.decode()
-		except Exception:
-			return self._data
+    @property
+    def text(self):
+        if isinstance(self._data, str):
+            return self._data
+        try:
+            return self._data.decode()
+        except Exception:
+            return self._data
 
     def json(self):
         if self._data:
