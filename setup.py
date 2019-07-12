f556cb8a0f049b88eecc066fd9a6c1f3aba8d3f7
Komissarov Andrey <komissar.off.andrey@gmail.com>
2019-07-12 18:10:12 +0300

    Fixes & Updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='k2',
-    version='1.2.1',
+    version='1.2.2',
     author='Komissarov Andrey',
     author_email='Komissar.off.andrey@mail.ru',
     description='Framework for simple services',
