0458e065192b12e8112208101951fb554910caf3
Komissarov Andrey <komissar.off.andrey@gmail.com>
2019-07-30 12:15:17 +0300

    upd statsCGI

--- k2/stats/aeon_module/site_module.py
+++ k2/stats/aeon_module/site_module.py
@@ -13,27 +13,42 @@ from k2.stats import (
     export,
     reset,
 )
+from k2.utils.autocfg import AutoCFG
+from k2.utils.http import NOT_FOUND
 
 STATUS_OK = dumps({'status': 'ok'})
 
 
 class StatsCGI(SiteModule):
-    def get(self, req):
-        return Response(
-            data=dumps(
-                {
-                    'status': 'ok',
-                    'data': export(),
+    defautls = {
+        'localips': False,
+    }
+
+    def __init__(self, **kwargs):
+        self.cfg = AutoCFG(kwargs).update_missing(self.defautls)
+
+    async def get(self, req):
+        if not self.cfg.localips or (self.cfg.localips and req.is_local()):
+            return Response(
+                data=dumps(
+                    {
+                        'status': 'ok',
+                        'data': export(),
+                    }
+                ),
+                code=200,
+                headers={
+                    'Content-Type': 'application/json; charset=utf-8',
                 }
-            ),
-            code=200,
-            headers={
-                'Content-Type': 'application/json; charset=utf-8',
-            }
-        )
+            )
+        else:
+            return Response(code=404, data=NOT_FOUND)
 
     async def post(self, req):
-        await reset(
-            key=req.args.get('key', None)
-        )
-        return Response(code=200, data=STATUS_OK)
+        if not self.cfg.localips or (self.cfg.localips and req.is_local()):
+            await reset(
+                key=req.args.get('key', None)
+            )
+            return Response(code=200, data=STATUS_OK)
+        else:
+            return Response(code=404, data=NOT_FOUND)
